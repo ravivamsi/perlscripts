4e78e0ee4ae6819331608cc89fd22eaa02da23c1
Vamsi Ravi <Vamsi.Ravi@hilton.com>
2019-01-29 17:17:44 -0600

    Sample Requests in place

---
+++ ApiProgramming/postexample.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;  
+
+my $uri = 'https://orbit.theplanet.com/Login.aspx?url=/Default.aspx';
+my $json = '{"username":"foo","password":"bar"}';
+my $req = HTTP::Request->new( 'POST', $uri );
+$req->header( 'Content-Type' => 'application/json' );
+$req->content( $json );
+
+my $lwp = LWP::UserAgent->new;
+my $response = $lwp->request( $req );
+
+if ( $response->is_success() ) {
+    print("Body: " . $response->decoded_content);
+}
+else {
+    print("ERROR Status Code: " . $response->status_line());
+}
