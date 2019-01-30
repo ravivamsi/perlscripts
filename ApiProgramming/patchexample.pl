94f591705578ed121eadabd304f48c6bbb38a8c2
Vamsi Ravi <Vamsi.Ravi@hilton.com>
2019-01-29 21:48:05 -0600

    PATCH Example is complete

--- ApiProgramming/patchexample.pl
+++ ApiProgramming/patchexample.pl
@@ -2,10 +2,15 @@
 use strict;
 use warnings;
 
-my $uri = 'https://orbit.theplanet.com/Login.aspx?url=/Default.aspx';
-my $json = '{"username":"foo","password":"bar"}';
-my $req = HTTP::Request->new( 'POST', $uri );
-$req->header( 'Content-Type' => 'application/json' );
+use HTTP::Request;
+use LWP::UserAgent;
+
+$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME}=0;
+
+my $uri = 'https://jsonplaceholder.typicode.com/posts/1';
+my $json = '{"title": "fool", "body": "bar", "userId": 1}';
+my $req = HTTP::Request->new( 'PATCH', $uri );
+$req->header( 'Content-Type' => 'application/json; charset=UTF-8' );
 $req->content( $json );
 
 my $lwp = LWP::UserAgent->new;
@@ -13,6 +18,10 @@ my $response = $lwp->request( $req );
 
 if ( $response->is_success() ) {
     print("Body: " . $response->decoded_content);
+    print("\n");
+    print("Status Code: " . $response->code());
+    print("\n");
+    print ("Headers: ". $response->headers()->as_string);
 }
 else {
     print("ERROR Status Code: " . $response->status_line());
