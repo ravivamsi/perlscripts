e91070b4afe925f0c98c5b596497e375d444967c
Vamsi Ravi <Vamsi.Ravi@hilton.com>
2019-01-29 21:51:51 -0600

    HEAD HTTP Method Example is complete

--- ApiProgramming/headexample.pl
+++ ApiProgramming/headexample.pl
@@ -2,18 +2,25 @@
 use strict;
 use warnings;
 
-my $uri = 'https://orbit.theplanet.com/Login.aspx?url=/Default.aspx';
-my $json = '{"username":"foo","password":"bar"}';
-my $req = HTTP::Request->new( 'POST', $uri );
-$req->header( 'Content-Type' => 'application/json' );
-$req->content( $json );
+use HTTP::Request;
+use LWP::UserAgent;
+
+$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME}=0;
+
+my $uri = 'https://jsonplaceholder.typicode.com/todos/1';
+
+my $req = HTTP::Request->new( 'HEAD', $uri );
 
 my $lwp = LWP::UserAgent->new;
 my $response = $lwp->request( $req );
 
 if ( $response->is_success() ) {
-    print("Body: " . $response->decoded_content);
-}
-else {
+  print("Body: " . $response->decoded_content);
+  print("\n");
+
+  print("Status Code: " . $response->code());
+  print("\n");
+  print ("Headers: ". $response->headers()->as_string);
+}else {
     print("ERROR Status Code: " . $response->status_line());
 }
